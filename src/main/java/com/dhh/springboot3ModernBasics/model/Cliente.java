c8cb15639bf21f88c6db3d6ce4783f1b03d742c4
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-05-31 16:11:35 +0200

    Update cliente con lombock

--- src/main/java/com/dhh/springboot3ModernBasics/model/Cliente.java
+++ src/main/java/com/dhh/springboot3ModernBasics/model/Cliente.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,36 +19,4 @@ public class Cliente {
     private String nombre;
     private String email;
 
-    public Cliente() {
-    }
-
-    public Cliente(Long id, String nombre, String email) {
-        this.id = id;
-        this.nombre = nombre;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
